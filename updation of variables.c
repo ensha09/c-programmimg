82eab7aa94909ad691a696d978ef658b942d2e72
ensha09 <sheikhinshainsha879@gmail.com>
2025-08-23 20:47:39 +0530

    Add files via upload

---
+++ updation of variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    x = 5;
+    printf("%d\n", x);
+    x = 6;
+    printf("%d\n", x);
+    x = x + 7;
+    printf("%d\n", x);
+
+    x = x - 12;
+    printf("%d", x);
+    return 0;
+}
